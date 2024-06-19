0e9e177366036db4ecc3b36ed83bf848117d12b4
KingRainbow44 <kobedo11@gmail.com>
2024-06-19 14:51:40 -0400

    feat(loot): Always make normal trial keys go into player loot

---
+++ src/main/java/moe/seikimo/mwhrd/mixin/ItemEntityMixin.java
@@ -0,0 +1,43 @@
+package moe.seikimo.mwhrd.mixin;
+
+import moe.seikimo.mwhrd.TrialChamberLoot;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.stat.Stats;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemEntity.class)
+public abstract class ItemEntityMixin {
+    @Shadow public abstract ItemStack getStack();
+
+    @Inject(method = "onPlayerCollision", at = @At(
+        value = "INVOKE",
+        target = "Lnet/minecraft/entity/player/PlayerInventory;insertStack(Lnet/minecraft/item/ItemStack;)Z"
+    ), cancellable = true)
+    public void checkIfKey(PlayerEntity player, CallbackInfo ci) {
+        var itemStack = this.getStack();
+        var item = itemStack.getItem();
+        var count = itemStack.getCount();
+
+        if (item != Items.TRIAL_KEY) return;
+
+        // Add the key to the player's loot.
+        TrialChamberLoot.addLoot(player.getUuid(), itemStack);
+
+        // Run default behavior.
+        var thisEntity = (ItemEntity) (Object) this;
+        player.sendPickup(thisEntity, count);
+        player.increaseStat(Stats.PICKED_UP.getOrCreateStat(item), count);
+        player.triggerItemPickedUpByEntityCriteria(thisEntity);
+
+        thisEntity.discard();
+
+        ci.cancel();
+    }
+}
