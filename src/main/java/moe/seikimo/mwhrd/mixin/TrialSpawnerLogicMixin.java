f471be295d0063941f5688091c74d2fa43f095ad
KingRainbow44 <kobedo11@gmail.com>
2024-06-21 10:21:53 -0400

    feat(trial): Grant ominous trial monsters stat and gear buffs

--- src/main/java/moe/seikimo/mwhrd/mixin/TrialSpawnerLogicMixin.java
+++ src/main/java/moe/seikimo/mwhrd/mixin/TrialSpawnerLogicMixin.java
@@ -1,9 +1,19 @@
 package moe.seikimo.mwhrd.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import moe.seikimo.mwhrd.MobGear;
+import moe.seikimo.mwhrd.MyWellHasRunDry;
 import moe.seikimo.mwhrd.TrialChamberLoot;
 import moe.seikimo.mwhrd.interfaces.ITrialSpawnerUtils;
 import net.minecraft.block.spawner.TrialSpawnerData;
 import net.minecraft.block.spawner.TrialSpawnerLogic;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.mob.*;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.loot.LootTable;
 import net.minecraft.loot.context.LootContextParameterSet;
 import net.minecraft.loot.context.LootContextTypes;
@@ -16,16 +26,27 @@ import net.minecraft.world.WorldEvents;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 @Mixin(TrialSpawnerLogic.class)
 public abstract class TrialSpawnerLogicMixin implements ITrialSpawnerUtils {
+    @Unique private static final Set<Class<? extends MobEntity>> BLACKLIST = Set.of(
+        CaveSpiderEntity.class,
+        BreezeEntity.class
+    );
+
     @Final @Shadow private TrialSpawnerData data;
+
+    @Shadow
+    public abstract boolean isOminous();
 
     @Override
     public Set<UUID> mwhrd$getSpawnerPlayers() {
@@ -60,5 +81,41 @@ public abstract class TrialSpawnerLogicMixin implements ITrialSpawnerUtils {
         world.syncWorldEvent(WorldEvents.TRIAL_SPAWNER_EJECTS_ITEM, pos, 0);
 
         ci.cancel();
+    }
+
+    @Inject(method = "trySpawnMob", at = @At("TAIL"))
+    public void onMobSpawned(
+        ServerWorld world, BlockPos pos, CallbackInfoReturnable<Optional<UUID>> cir,
+        @Local(ordinal = 0) Entity spawnedEntity
+    ) {
+        if (!this.isOminous()) return;
+        if (!(spawnedEntity instanceof MobEntity mob)) return;
+
+        // Apply full armor to the mob.
+        if (!BLACKLIST.contains(mob.getClass())) {
+            mob.equipStack(EquipmentSlot.HEAD, MobGear.HELMET.copy());
+            mob.equipStack(EquipmentSlot.CHEST, MobGear.CHESTPLATE.copy());
+            mob.equipStack(EquipmentSlot.LEGS, MobGear.LEGGINGS.copy());
+            mob.equipStack(EquipmentSlot.FEET, MobGear.BOOTS.copy());
+
+            // Apply mob effects.
+            mob.addStatusEffect(new StatusEffectInstance(
+                StatusEffects.STRENGTH, Integer.MAX_VALUE
+            ));
+            mob.addStatusEffect(new StatusEffectInstance(
+                StatusEffects.SPEED, Integer.MAX_VALUE, 1
+            ));
+        }
+
+        if (mob instanceof AbstractSkeletonEntity) {
+            mob.equipStack(EquipmentSlot.MAINHAND, MobGear.BOW.copy());
+            mob.equipStack(EquipmentSlot.OFFHAND, MobGear.ARROWS.copy());
+        } else {
+            mob.equipStack(EquipmentSlot.MAINHAND, MobGear.SWORD.copy());
+        }
+
+        if (MyWellHasRunDry.getRandom().nextInt(0, 5) == 0) {
+            mob.equipStack(EquipmentSlot.OFFHAND, new ItemStack(Items.TOTEM_OF_UNDYING));
+        }
     }
 }
