4c3918edebb90f6d0525f2e0c0772049632a0d88
KingRainbow44 <kobedo11@gmail.com>
2024-06-22 22:41:24 -0400

    feat(balance): Allow potions to stack to 64

---
+++ src/main/java/moe/seikimo/mwhrd/mixin/PotionItemMixin.java
@@ -0,0 +1,17 @@
+package moe.seikimo.mwhrd.mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.PotionItem;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PotionItem.class)
+public abstract class PotionItemMixin extends Item {
+    public PotionItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public int getMaxCount() {
+        return 64;
+    }
+}
