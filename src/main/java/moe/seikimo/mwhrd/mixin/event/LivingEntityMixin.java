d7b7c8800e45a3f59f4ef0682e39aba6a71ff2d2
KingRainbow44 <kobedo11@gmail.com>
2025-01-01 21:49:57 -0500

    fix(LivingEntityMixin.java): Prevent entity from dying

--- src/main/java/moe/seikimo/mwhrd/mixin/event/LivingEntityMixin.java
+++ src/main/java/moe/seikimo/mwhrd/mixin/event/LivingEntityMixin.java
@@ -8,12 +8,21 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
+    @Shadow protected boolean dead;
+
+    @Shadow
+    public abstract void setHealth(float health);
+
+    @Shadow
+    public abstract float getMaxHealth();
+
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
@@ -36,6 +45,9 @@ public abstract class LivingEntityMixin extends Entity {
             .onPreDeath((LivingEntity) (Object) this, source);
         if (!result) {
             cir.setReturnValue(false);
+
+            this.dead = false;
+            this.setHealth(this.getMaxHealth());
         }
     }
 }
