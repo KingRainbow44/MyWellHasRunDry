3a01dd3149bbad679847e3a2785f42805c7cd05e
KingRainbow44 <kobedo11@gmail.com>
2025-01-23 21:39:52 -0500

    misc(fabric.mod.json): add client mod initializer

---
+++ src/main/java/moe/seikimo/mwhrd/client/MyWellHasRunDry.java
@@ -0,0 +1,10 @@
+package moe.seikimo.mwhrd.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class MyWellHasRunDry implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
