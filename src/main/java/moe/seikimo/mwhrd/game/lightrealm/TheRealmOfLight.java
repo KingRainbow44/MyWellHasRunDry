f06b446cb9e3fad653df9783eda6af9dc310e1d4
KingRainbow44 <kobedo11@gmail.com>
2025-05-23 01:03:07 -0400

    fix(custom/rol): reduce transport time to half second
    this is to reduce bug abuse

--- src/main/java/moe/seikimo/mwhrd/game/lightrealm/TheRealmOfLight.java
+++ src/main/java/moe/seikimo/mwhrd/game/lightrealm/TheRealmOfLight.java
@@ -42,7 +42,7 @@ import java.util.function.BooleanSupplier;
 @Slf4j
 public final class TheRealmOfLight extends RuntimeWorld {
     public static final long MAX_TICKS = Ticks.ofHours(1);
-    private static final long TRANSPORT_WAIT = Ticks.ofSeconds(5);
+    private static final long TRANSPORT_WAIT = 5;
 
     private static final Vec3d BOSS_SPAWN_POS = new Vec3d(1, 410, 0);
 
